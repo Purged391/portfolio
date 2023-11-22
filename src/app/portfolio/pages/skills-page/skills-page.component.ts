597725dca6ee5b2997ac3f802deff3f25ca3e688
Mario Dominguez Garcia <mariodgxiii@gmail.com>
2023-11-22 11:21:13 +0100

    [MDG] Initial Project Page Full Structure

---
+++ src/app/portfolio/pages/skills-page/skills-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-skills-page',
+  templateUrl: './skills-page.component.html',
+  styleUrl: './skills-page.component.scss'
+})
+export class SkillsPageComponent {
+
+}
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    ContactPageComponent,
+    HomePageComponent,
+    ProjectsPageComponent,
+    SkillsPageComponent,
   ],
   imports: [
     CommonModule,
