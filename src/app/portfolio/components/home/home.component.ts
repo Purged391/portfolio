69f2e66440624127725045155c57b920b045ceda
Mario Dominguez Garcia <mariodgxiii@gmail.com>
2024-10-02 21:29:55 +0200

    [MDG] Home Component

--- src/app/portfolio/components/home/home.component.ts
+++ src/app/portfolio/components/home/home.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import TranslatePipe from '../../../pipes/translate.pipe';
+
 
 @Component({
   selector: 'portfolio-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    TranslatePipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export default class HomeComponent {
-
+  public navigateTo(event: MouseEvent, dest: string): void{
+    const target = event.target as HTMLElement;
+    target.blur();
+  }
 }
