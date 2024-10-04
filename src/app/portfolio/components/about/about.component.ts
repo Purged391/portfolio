9c6aa93559304ba7531c3bce4e478a1148a9d595
Mario Dominguez Garcia <mariodgxiii@gmail.com>
2024-10-04 21:25:46 +0200

    [MDG] Animate more containers

--- src/app/portfolio/components/about/about.component.ts
+++ src/app/portfolio/components/about/about.component.ts
@@ -21,7 +21,7 @@ import { CardModule } from 'primeng/card';
       ]),
       transition('visible => hidden', [
         style({ height: '*', opacity: 1 }),  // Empieza con altura 0
-        animate('600ms ease-out', style({ height: '0', opacity: 0 })) // Se expande a su altura completa
+        animate('600ms ease-out', style({ height: '*', opacity: 0 })) // Se expande a su altura completa
       ])
     ]),
     // Animación para el p-card que entra desde la izquierda
@@ -33,46 +33,70 @@ import { CardModule } from 'primeng/card';
       transition('visible => hidden', [
         style({ transform: 'translateX(0)', opacity: 1 }), // Empieza desde fuera de la vista
         animate('500ms 300ms ease-out', style({ transform: 'translateX(-100%)', opacity: 0 })) // Entra con un retardo
-      ])
+      ]),
+    ]),
+    trigger('slideRight', [
+      transition('hidden => visible', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }), // Empieza desde fuera de la vista
+        animate('500ms 300ms ease-out', style({ transform: 'translateX(0)', opacity: 1 })) // Entra con un retardo
+      ]),
     ])
   ]
 })
 export default class AboutComponent implements AfterViewInit {
 
-  public inView = signal<boolean>(false);
-  public isCardVisible = signal<string>('hidden'); // Nueva variable para controlar la visibilidad de la tarjeta
+  public inView = signal<boolean[]>([false, false, false, false]);  // Controla la visibilidad de los containers
+  public isCardVisible = signal<string[]>(['hidden', 'hidden', 'hidden', 'hidden']); // Controla la visibilidad de los p-card
+
 
 
   constructor(private el: ElementRef, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit() {
-    this.inView.set(false);
+
     if (isPlatformBrowser(this.platformId)) {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-           // console.log('El elemento está en el viewport!');
-            this.inView.set(true);
+      const targetElements = this.el.nativeElement.querySelectorAll('.box');
 
-            setTimeout(() => {
-              this.isCardVisible.set('visible');
-            }, 600);
 
-          } else {
-            const rect = entry.boundingClientRect;
-            if (rect.top < 0) {
-            } else if (rect.top > 0) {
-              this.isCardVisible.set('hidden');
-              this.inView.set(false);
+      targetElements.forEach((targetElement: HTMLElement, index: number) => {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              const currentView =  this.inView();
+              const cloneView =  [...currentView];
+              cloneView[index] = true;
+              this.inView.set(cloneView);
+               setTimeout(() => {
+                 const currentCard =  this.isCardVisible();
+                 const cloneCard =  [...currentCard];
+                 cloneCard[index] = 'visible';
+                 this.isCardVisible.set(cloneCard);
+               }, 600);
+
+            } else {
+              const rect = entry.boundingClientRect;
+              console.log(index)
+              console.log(rect)
+              if (rect.top < 0) {
+
+              } else if ((rect.top > 0 && index%2 === 0) || (rect.top -100 > 0 && index%2 !== 0)) {
+                const currentCard =  this.isCardVisible();
+                const cloneCard =  [...currentCard];
+                cloneCard[index] = 'hidden';
+                this.isCardVisible.set(cloneCard);
+                const currentView =  this.inView();
+                const cloneView =  [...currentView];
+                cloneView[index] = false;
+                this.inView.set(cloneView);
+              }
             }
-          }
+          });
+        }, {
+          threshold: index === 0 ? 0 : 1
         });
-      }, {
-        threshold: 0
-      });
 
-      const targetElement = this.el.nativeElement.querySelector('#myElement');
-      observer.observe(targetElement);
+        observer.observe(targetElement);
+      });
     }
   }
 
