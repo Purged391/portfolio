72c6b544b51b8fa8596867f6dca8780579c81c63
Mario Dominguez Garcia <mariodgxiii@gmail.com>
2024-12-16 20:31:13 +0100

    [MDG] Added Spherical Stars and resolve conflicts

--- src/app/portfolio/components/contact/contact.component.ts
+++ src/app/portfolio/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild, inject, PLATFORM_ID, AfterViewInit, OnDestroy } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'portfolio-contact',
@@ -13,6 +14,7 @@ export default class ContactComponent implements AfterViewInit, OnDestroy {
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
+  private controls!: OrbitControls;
   private animationId!: number;
 
   public ngAfterViewInit() {
@@ -24,8 +26,8 @@ export default class ContactComponent implements AfterViewInit, OnDestroy {
 
   public ngOnDestroy() {
     if (this.platformId === 'browser') {
-      window.removeEventListener('resize', this.onWindowResize.bind(this), false);
       cancelAnimationFrame(this.animationId);
+      window.removeEventListener('resize', this.onWindowResize.bind(this), false);
     }
   }
 
@@ -35,25 +37,31 @@ export default class ContactComponent implements AfterViewInit, OnDestroy {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x000000);
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.camera.position.z = 5;
 
-    const circleGeometry = new THREE.CircleGeometry(0.5, 64);
-    const material = new THREE.MeshBasicMaterial({ color: 0x888888 });
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 500;
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.25;
+    this.controls.enableZoom = false;
+
+    const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32); // Cambia a geometría de esfera
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
     for (let i = 0; i < 10000; i++) {
-      const circle = new THREE.Mesh(circleGeometry, material);
-      circle.position.set(
+      const sphere = new THREE.Mesh(sphereGeometry, material);
+      sphere.position.set(
         THREE.MathUtils.randFloatSpread(2000), // x
         THREE.MathUtils.randFloatSpread(2000), // y
         THREE.MathUtils.randFloatSpread(2000)  // z
       );
-      this.scene.add(circle);
+      this.scene.add(sphere);
     }
 
     const animate = () => {
       this.animationId = requestAnimationFrame(animate);
+      this.controls.update();
       this.renderer.render(this.scene, this.camera);
     };
 
