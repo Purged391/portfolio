b54d21c84c26c214950c080b3967dfa359248bb5
Mario Dominguez Garcia <mariodgxiii@gmail.com>
2024-12-26 11:21:18 +0100

    [MDG] Add earth 3d

--- src/app/portfolio/components/contact/contact.component.ts
+++ src/app/portfolio/components/contact/contact.component.ts
@@ -33,34 +33,65 @@ export default class ContactComponent implements AfterViewInit, OnDestroy {
 
   private createScene() {
     const canvas = this.canvasRef.nativeElement;
-    this.renderer = new THREE.WebGLRenderer({ canvas });
+    this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.camera.position.z = 500;
+    this.camera.position.z = 4;
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.enableDamping = true;
     this.controls.dampingFactor = 0.25;
     this.controls.enableZoom = false;
 
-    const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32); // Cambia a geometría de esfera
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const sunLight = new THREE.DirectionalLight(0xffffff);
+    sunLight.position.set(-2, 0.5, 1);
+    this.scene.add(sunLight);
 
-    for (let i = 0; i < 10000; i++) {
-      const sphere = new THREE.Mesh(sphereGeometry, material);
-      sphere.position.set(
-        THREE.MathUtils.randFloatSpread(2000), // x
-        THREE.MathUtils.randFloatSpread(2000), // y
-        THREE.MathUtils.randFloatSpread(2000)  // z
-      );
-      this.scene.add(sphere);
-    }
+    const loader = new THREE.TextureLoader();
+
+    const earthGroup = new THREE.Group();
+
+    const geometry = new THREE.IcosahedronGeometry(1, 12);
+    const material = new THREE.MeshStandardMaterial({
+      map: loader.load('assets/images/earth/earth_color_10K.png'),
+    });
+    const earthMesh = new THREE.Mesh(geometry, material);
+    earthGroup.add(earthMesh);
+    earthGroup.rotation.z = -23.4 * Math.PI / 180;
+
+    const lightsMat = new THREE.MeshBasicMaterial({
+      map: loader.load('assets/images/earth/earthlights1k.jpg'),
+      blending: THREE.AdditiveBlending,
+     });
+    const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+    earthGroup.add(lightsMesh);
+
+    const cloudsMat = new THREE.MeshStandardMaterial({
+      map: loader.load('assets/images/earth/earth_clouds_8K.png'),
+      blending: THREE.AdditiveBlending,
+    });
+    const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+    cloudsMesh.scale.setScalar(1.02);
+    earthGroup.add(cloudsMesh);
+
+    this.scene.add(earthGroup);
+
+
+    const sunGeometry = new THREE.SphereGeometry(5, 32, 32);
+    const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+    sunMesh.position.set(-100, 30, 50);
+    this.scene.add(sunMesh);
+
 
     const animate = () => {
       this.animationId = requestAnimationFrame(animate);
+      earthMesh.rotation.y += 0.0008;
+      lightsMesh.rotation.y += 0.0008;
+      cloudsMesh.rotation.y += 0.00099;
       this.controls.update();
       this.renderer.render(this.scene, this.camera);
     };
